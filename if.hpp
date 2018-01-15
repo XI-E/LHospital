e0b1f19394758b5b52b2169816643864802621c2
Anirudh Panigrahi <chiru9670@gmail.com>
2018-01-15 20:23:46 +0530

    Administrator employee management inplemented partially
    1. Defined << operator overloads to manage printing of data stored in enums
    2. Added age updation for all employees
    3. Generalized get_employee_by_id() and write() functions by using void pointers
    4. Fixed default constructors of doctor and nurse
    5. Added getters for doctor and nurse data members
    6. Added functionality for displaying employee data by administrator (implemented for general employees and doctors for now)

--- if.hpp
+++ if.hpp
@@ -1,22 +1,35 @@
 #ifndef INTERFACE_HPP
 #define INTERFACE_HPP
 
-#include "ui/ui.hpp"
 
 class interface{
 	public:
-		static void interface_init();
+		static void init();
 		static int login_screen();
 		static int menu();
 		static void patient_management();
 		static void employee_management();
 		static void stock_management();
+		
+		static void doctor_screen();
+		static void nurse_screen();
+		static void receptionist_screen();
 
 		static void error(char*);
 		static void clear_error();
+		
+		static int log_this(char *);
 
 	private:
 		interface();
+		class validate_menu			//for creating a validation function to use in menus
+		{							//to validate the choice input of the menu option to be accessed
+				static int lowest_choice, greatest_choice;
+				validate_menu();
+			public:
+				static int input(const char *);
+				static void set_menu_limits(int, int);
+		};
 		static box window;
 };
 
